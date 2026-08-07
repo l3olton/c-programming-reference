b9e8f2a15963febc55a0883760701001213dbaee
Thomas Bolton <tbolton96@icloud.com>
2026-08-07 21:52:35 +0100

    shorten ring buffer usage example

--- ring_buffer/main.c
+++ ring_buffer/main.c
@@ -5,47 +5,37 @@
 
 int main(void)
 {
-    const size_t buf_size = 8;
+    const size_t buf_size = 12;
     RingBuffer rb = {0};
     uint8_t buffer[buf_size];
 
     ring_buf_init(&rb, buffer, buf_size);
 
-    for (uint8_t i = 'A'; i <= 'J'; i++) {
-        bool can_push = ring_buf_push(&rb, i);
-        ring_buf_visualize(&rb);
-        printf("can_push: %d\n\n", can_push);
-    }
-
     uint8_t out;
 
-    for (size_t i = 0; i < 3; i++) {
-        if (ring_buf_pop(&rb, &out)) {
-            printf("pop: %c\n", out);
-            ring_buf_visualize(&rb);
-            putchar('\n');
-        }
-    }
-
-    for (uint8_t i = 'K'; i <= 'T'; i++) {
-        bool can_push = ring_buf_push(&rb, i);
-        ring_buf_visualize(&rb);
-        printf("can_push: %d\n\n", can_push);
-    }
-
-    while (ring_buf_pop(&rb, &out));
+    uint8_t str1[] = "subhanallah";
+    for (size_t i = 0; i < sizeof(str1) / sizeof(str1[0]); i++)
+        ring_buf_push(&rb, str1[i]);
 
     ring_buf_visualize(&rb);
     putchar('\n');
 
-    uint8_t message[] = "risalah";
-    for (size_t i = 0; i < sizeof(message) / sizeof(message[0]); i++)
-        ring_buf_push(&rb, message[i]);
+    printf("Pop 6 elements:\n");
+    size_t i = 0;
+    while (i++ < 6 && ring_buf_pop(&rb, &out))
+        putchar(out);
+    putchar('\n');
 
     ring_buf_visualize(&rb);
     putchar('\n');
 
-    printf("Pop all elements:\n");
+    uint8_t str2[] = " akbar";
+    for (size_t i = 0; i < sizeof(str2) / sizeof(str2[0]); i++)
+        ring_buf_push(&rb, str2[i]);
+
+    ring_buf_visualize(&rb);
+    putchar('\n');
+
     while (ring_buf_pop(&rb, &out))
         putchar(out);
     putchar('\n');
